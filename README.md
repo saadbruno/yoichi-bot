21a9228cdac4bd86ffcb8cdad771b7964b2dd8a5
Bruno Saad <saadbruno@gmail.com>
2021-05-28 21:55:14 +0000

    Criado comando que pega paginas de mondolis no airtable

--- README.md
+++ README.md
@@ -1,18 +1,32 @@
 # Yoichi Bot
-Um bot do Discord que reage a menções de seu nome. Feito de brincadeira para o Discord do [Monastério Gentileza](https://discord.gg/cyWp3KEwtc), de Monge Han.
+Um bot feito de brincadeira para o Discord do [Monastério Gentileza](https://discord.gg/cyWp3KEwtc), de Monge Han, personificando o Yoichi e adicionando funções extras.
 
 ![screenshot do bot em funcionamento](https://user-images.githubusercontent.com/23201434/111733997-ef1b2d00-8857-11eb-85e9-3550ddc9c4cf.png)
+
+## Recursos:
+- Reações a menções de "Yoichi", "Bom dia, Yoichi", entre outros
+- Páginas de mondolís. Ex: `!mondolis cap 1 pag 2`
 
 ## Desenvolvimento:
 Pull requests são muito bem vindas!
 
-Requerimentos:
+### Requerimentos:
 - [Node.js](https://nodejs.org/)
 - Um Aplicativo com Bot no [portal de desenvolvimento do Discord](https://discord.com/developers/applications)
+- Uma base (planilha) no Airtable seguindo [esse schema](https://airtable.com/shr4aG6NiuZKNQ7Az), você pode duplicar essa base, caso necessário!
 
-Dev environment:
+### Dev environment:
 - Clone o repositório
 - Copie o `config.json.example` para `config.json`
-- Edite a linha `token` pra incluir o seu token pessoal do bot criado no passo anterior
+- Edite a `config.json` com os dados necessários
+  - `token`: Token pessoal do bot criado no [portal de desenvolvimento do Discord](https://discord.com/developers/applications)
+  - `servidoresAutorizados`: Alguns comandos (como o `!avatar`) necessitam que você seja administrador de algum dos servidores listados aqui.
+  - `emoteBrabo` e `emoteEnvergonhado`: São os emotes que o Yoichi reage em casos específicos. Você pode incluir um emoji aqui (como no exemplo) ou algum emote personalizado do Discord (ex: `:yoichibrabo:822537986428239893`)
+  - `airtableKey`: Sua chave de API do Airtable, disponível [na sua conta](https://airtable.com/account)
+  - `airtableBase`: O ID da sua base (planilha) do Airtable, disponível na [documentação do Airtable](https://airtable.com/api)
 - Instale as dependencias com `npm install`
 - Rode o bot com o comando `node index.js`
+
+## Links úteis
+- [Discord.js](https://discord.js.org/)
+- [Airtable.js](https://github.com/Airtable/airtable.js)
--- comandos/mondolis.js
+++ comandos/mondolis.js
@@ -1,7 +1,101 @@
+const config = require("../config.json");
+var Airtable = require('airtable');
+// Airtable.configure({
+// 	endpointUrl: 'https://api.airtable.com',
+// 	apiKey: config.airtableKey
+// });
+// var base = Airtable.base(config.airtableBase);
+
+
+var Airtable = require('airtable');
+var base = new Airtable({ apiKey: 'key7IA2T4uMnnn1sC' }).base('app1lSsv8iVHBQjVZ');
+
 module.exports = {
 	name: 'mondolis',
 	description: 'Posta o link de Mondolís no Tapas',
 	execute(message, args) {
-		message.channel.send('Leia Mondolís!\nhttps://tapas.io/series/Mondolis-PTBR/info');
+
+		// se o usuário não incluiu nenhuma página, apenas posta o link do tapas
+		if (!args.length) {
+			return message.channel.send('Leia Mondolís!\nhttps://tapas.io/series/Mondolis-PTBR/info');
+		}
+
+		// o usuário também pode pedir uma pagina específica, que bisca numa planilha do AirTable
+
+		// vamos primeiro formatar os argumentos considerando diversos inputs diferentes do usuário
+		// exemplo: !mondolis c 1 p 2
+		//			!mondolis cap 1 pag 2
+		//			!mondolis capitulo 1 pagina 2
+		// Todos esses comandos devem funcioanr igualmente
+
+		// pagCap é o objeto que contém o número do capítulo e da página que o usuário enviou, e será usado pra buscar a página no Airtable.
+		// o formato final do pagCap deve ser {"c": "X", "p": "Y"}, onde X e Y são números
+		var pagCap = {};
+
+		// caso o primeiro argumento seja tudo junto, tipo c1p2 ou c2p30
+		if (/c([0-9]+)p([0-9]+)/i.test(args[0])) {
+			console.log(args[0]);
+			const cpSplit = args[0].split(/c|p/);
+			console.log(cpSplit);
+			pagCap = {
+				"c": cpSplit[1],
+				"p": cpSplit[2]
+			};
+		} else {
+
+		// argumento não foi tudo junto. Deve ser separado então. Vamos padronizar "cap", "capítulo", "pag", "página", etc
+			for (var i = 0; i < args.length; i++) {
+				switch (args[i].toLowerCase()) {
+					case 'capitulo':
+					case 'capítulo':
+					case 'cap':
+						args[i] = 'c';
+						break;
+
+					case 'página':
+					case 'pagina':
+					case 'pág':
+					case 'pag':
+						args[i] = 'p';
+						break;
+
+					default:
+						break;
+				}
+			}
+
+			// agora vamos associar o número enviado ao lado de cada um dos argumentos (página e capítulo)
+			for (let i = 0; i < args.length; i++) {
+				if (args[i] == 'p' || args[i] == 'c') {
+					const j = i + 1;
+					pagCap[args[i]] = args[j];
+				}
+			}
+
+		}
+
+		// Verifica se temos argumentos c e p
+		if (!pagCap.c || !pagCap.p) {
+			console.error(`\n\n:: Faltando argumentos de capítulo ou página`)
+			return message.channel.send(`Defina uma página e capítulo, tipo \`!mondolis cap 3 pag 1\` <${config.emoteBrabo}>`);
+		}
+
+		// pega linha no airtable relativa à página e capítulos solicitados
+		base('mondolis').select({
+			maxRecords: 1,
+			view: 'Grid view',
+			filterByFormula: `AND({capitulo} = '${pagCap.c}', {pagina} = '${pagCap.p}', {url} != '') `,
+		}).firstPage(function (err, records) {
+			if (err) { console.error(err); return; }
+			if (!records.length) {
+				return message.channel.send(`Não encontrei essa página <${config.emoteBrabo}>`);
+			}
+			records.forEach(function (record) {
+				console.log('Retrieved', record.get('url'));
+				message.channel.send(`Mondolís: Capítulo ${pagCap.c}, Página ${pagCap.p}\n${record.get('url')}`)
+			});
+		});
+
+
 	},
 };
