8895a104d622b0d300853acf694a93709da4640b
Bruno Saad <saadbruno@gmail.com>
2021-05-29 16:44:43 +0000

    Agora o bot responde no canal se o comando for invocado por um usuário

--- comandos/aniversario.js
+++ comandos/aniversario.js
@@ -7,9 +7,21 @@ module.exports = {
 	name: 'aniversario',
 	description: 'aniversario!',
 	execute(message, args, client) {
+
+        var canalResposta;
+
+        // Esse copmando pode ser executado via Cron. Nesses casos, não existe um message.channel pra responder, então a gente usa o canal configurado no config.json
+        if (message) {
+            console.log('\n:: [aniversario] Comando foi enviado por um usuário. Respondendo no canal que o comando foi executado.');
+            canalResposta = message.channel;
+        } else {
+            console.log(`\n:: [aniversario] Comando foi executado via Cron. Postando no canalAniversario (${config.canalAniversario})`);
+            canalResposta = client.channels.cache.get(config.canalAniversario);
+        }
+
+        // cria variaveis pra data de hoje. Elas são separadas porque fica mais fácil de veriifcar o dia e o mês separado do ano (a gente não liga muito pro ano)
 		var dia = moment().tz('America/Sao_Paulo').format("D"); 
         var mes = moment().tz('America/Sao_Paulo').format("M"); 
-        var ano = moment().tz('America/Sao_Paulo').format("YYYY");
 
 		base('anivers').select({
             view: 'Grid view',
@@ -18,16 +30,16 @@ module.exports = {
         }).firstPage(function (err, records) {
             if (err) { console.error(err); return; }
             if (!records.length) {
-                return console.log("\n:: Não existem aniversário hoje! :(")
+                return console.log("\n:: [aniversario] Não existem aniversário hoje! :(")
             }
             records.forEach(function (record) {
 
+                console.log('\n:: [aniversario] Retirado do Airtable:', record.get('Nome'));
+                // formata a mensagem que será enviada
 				var mensagem = `Parabéns <@${record.get('Discord ID')}> <${config.emoteBrabo}>`;
+                // escolhe onde a mensagem será postada
+                canalResposta.send(mensagem);
 
-                console.log('\n:: Retrieved', record.get('dia'));
-				const cronChannel = client.channels.cache.get(config.canalAniversario);
-
-				cronChannel.send(mensagem)
             });
         });
 	},
