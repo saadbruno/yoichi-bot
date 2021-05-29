618a8a4063fba29ceac05bf53d017794bd74946e
Bruno Saad <saadbruno@gmail.com>
2021-05-29 22:15:27 +0000

    confere permissões

--- comandos/purgerole.js
+++ comandos/purgerole.js
@@ -9,6 +9,13 @@ module.exports = {
     name: 'purgerole',
     description: 'Remove todos os usuários de uma role específica.',
     execute(message, args, client) {
+
+        // se a mensagem veio de um usuário, veja se ele é autorizado
+        if (message && !(config.servidoresAutorizados.includes(message.channel.guild.id) && message.member.hasPermission('ADMINISTRATOR'))) {
+            console.log('\n\n:: [purgerole]: Usuário sem permissão executou o comando. Abortando.');
+            return;
+        }
+
         var guild = client.guilds.cache.get(config.guildAniversario);
         let role = guild.roles.cache.find(r => r.id === args[0]);
 
