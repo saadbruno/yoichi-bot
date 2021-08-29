9647eb77ffb9102652f5a93f01bdeb7eade1f770
Bruno Saad <saadbruno@gmail.com>
2021-08-29 17:05:57 +0000

    comando purgerole (via slash command e via cron tb)

--- comandos/purgerole.js
+++ comandos/purgerole.js
@@ -1,6 +1,4 @@
-const config = require("../config.json");
 const { Permissions } = require('discord.js');
-
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
@@ -14,22 +12,35 @@ module.exports = {
         ),
     async execute(interaction) {
 
-        if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) {
-            console.log('\n\n:: [purgerole]: Usuário sem permissão executou o comando. Abortando.');
-            return interaction.reply({ content: 'Você não tem permissão pra isso! Você não manda em mim!', ephemeral: true });
-        }
+        var role;
 
-        const role = interaction.options.getRole('role');
+        // aqui a gente divide o código em 2 partes pra pegar a role desejada, dependendo se o comando foi executado via cron, ou via interação
+        
+        // Se a interação NÃO possui um membro, ela veio o cron configurado no ../events/ready.js
+        // o cron 
+        if (!interaction.member) {
+            // console.log(interaction);
+            console.log('   :: [purgerole]: Executando via Cron');
+            role = interaction;
+        } else {
+
+            // checa se o membro é manager
+            if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) {
+                console.log('   :: [purgerole]: Usuário sem permissão executou o comando. Abortando.');
+                return interaction.reply({ content: 'Você não tem permissão pra isso! Você não manda em mim!', ephemeral: true });
+            }
+
+            role = interaction.options.getRole('role');
+            await interaction.reply({ content: `Removendo ${role.members.size} usuários da role ${role.name}.`, ephemeral: true });
+        }
 
         console.log(`   :: [purgerole] A role "${role.name}" possui ${role.members.size} membros no cache.`);
 
         role.members.forEach(member => {
             console.log(`   :: [purgerole] Removendo role ${role.name} do usuário ${member.user.tag}`);
             member.roles.remove(role)
-                .catch(error => console.error(`\n:: [purgerole] ERRO: Não foi possivel remover a role ${role.name} do usuário ${member.user.tag}. O bot tem permissão pra fazer isso?\n\n`, error));
+                .catch(error => console.error(`\n   :: [purgerole] ERRO: Não foi possivel remover a role ${role.name} do usuário ${member.user.tag}. O bot tem permissão pra fazer isso?\n\n`, error));
         });
-
-        await interaction.reply({ content: `Removendo ${role.members.size} usuários da role ${role.name}.`, ephemeral: true });
 
     },
 };
--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,32 @@
 const config = require("../config.json");
+const { CronJob } = require("cron");
 
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client) {       
+    execute(client) {
+
+        // posta mensagem de início 
         console.log(`========= YOICHI BOT =========\n:: Logado como ${client.user.tag}\n:: Online em ${client.guilds.cache.size} servidores \n==============================`);
 
-        client.guilds.cache.get(config.guildId).members.fetch().then((members) => {
+        let guild = client.guilds.cache.get(config.guildId);
+
+        // adiciona todos usuários ao cache do bit. No momento isso é utilizado no purgerole.
+        guild.members.fetch().then((members) => {
             console.log(`\n:: Adicionados ${members.size} usuários no cache`);
         });
+
+        // inicia os cronjobs
+        new CronJob(
+            "0 0 0 1 * *",
+            () => {
+                let role = guild.roles.cache.find(r => r.id === config.roleCounting);
+                client.commands.get("purgerole").execute(role);
+            },
+            null,
+            true,
+            "America/Sao_Paulo"
+        );
+
     },
 };
