e5c35e856c6d6e98a2857917da2ce35803f818b5
Bruno Saad <saadbruno@gmail.com>
2021-08-29 00:25:19 +0000

    migrado comando purgerole

--- comandos/purgerole.js
+++ comandos/purgerole.js
@@ -1,32 +1,35 @@
-// Comando que remove todos os usuários (em cache) de uma role específica, do servidor configurado no config.json
-// Limitações: No momento, o bot só remove as roles dos usuários que estão em cache.
-// É possível pegar todos os usuários de um servidor, mas não todos os usuários de uma role específica. E eu não sei se eu quero fazer isso
-// https://discord.js.org/#/docs/main/stable/class/GuildMemberManager?scrollTo=fetch
-
 const config = require("../config.json");
+const { Permissions } = require('discord.js');
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-    name: 'purgerole',
-    aliases: ['rolepurge'],
-    description: 'Remove todos os usuários de uma role específica.',
-    execute(message, args, client) {
+    data: new SlashCommandBuilder()
+        .setName('purgerole')
+        .setDescription('Remove todo mundo de uma determinada role')
+        .addRoleOption(option =>
+            option.setName('role')
+                .setDescription('A role / cargo pra tirar todo mundo')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
 
-        // se a mensagem veio de um usuário, veja se ele é autorizado
-        if (message && !(config.servidoresAutorizados.includes(message.channel.guild.id) && message.member.hasPermission('ADMINISTRATOR'))) {
+        if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) {
             console.log('\n\n:: [purgerole]: Usuário sem permissão executou o comando. Abortando.');
-            return;
+            return interaction.reply({ content: 'Você não tem permissão pra isso! Você não manda em mim!', ephemeral: true });
         }
 
-        var guild = client.guilds.cache.get(config.guildAniversario);
-        let role = guild.roles.cache.find(r => r.id === args[0]);
+        const role = interaction.options.getRole('role');
 
-        console.log(`:: [purgerole] A role "${role.name}" possui ${role.members.size} membros no cache.`);
+        console.log(`   :: [purgerole] A role "${role.name}" possui ${role.members.size} membros no cache.`);
 
         role.members.forEach(member => {
-            console.log(`:: [purgerole] Removendo role ${role.name} do usuário ${member.user.username}#${member.user.discriminator}`);
+            console.log(`   :: [purgerole] Removendo role ${role.name} do usuário ${member.user.tag}`);
             member.roles.remove(role)
-                .catch(error => console.error(`\n:: [purgerole] ERRO: Não foi possivel remover a role ${role.name} do usuário ${member.user.username}#${member.user.discriminator}. O bot tem permissão pra fazer isso?\n\n`, error));
+                .catch(error => console.error(`\n:: [purgerole] ERRO: Não foi possivel remover a role ${role.name} do usuário ${member.user.tag}. O bot tem permissão pra fazer isso?\n\n`, error));
         });
+
+        await interaction.reply({ content: `Removendo ${role.members.size} usuários da role ${role.name}.`, ephemeral: true });
 
     },
 };
--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,13 @@
+const config = require("../config.json");
+
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client) {
+    execute(client) {       
         console.log(`========= YOICHI BOT =========\n:: Logado como ${client.user.tag}\n:: Online em ${client.guilds.cache.size} servidores \n==============================`);
+
+        client.guilds.cache.get(config.guildId).members.fetch().then((members) => {
+            console.log(`\n:: Adicionados ${members.size} usuários no cache`);
+        });
     },
 };
